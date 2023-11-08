c78eec0e52cf5c9a6e2afa3ded4d5b583de1786f
Lucas <lucasmruiz05@gmail.com>
2023-11-07 21:02:17 -0300

    add dependencies and next cfg

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   experimental: {
     serverActions: true
-  }
+  },
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'v5.airtableusercontent.com',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
