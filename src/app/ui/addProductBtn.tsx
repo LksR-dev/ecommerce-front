6fde9525e7c828c71dad583631f02a2d7fa1de4d
Lucas <lucasmruiz05@gmail.com>
2023-11-07 21:14:24 -0300

    add ui productbtn

---
+++ src/app/ui/addProductBtn.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { useRecoilState } from 'recoil';
+import { cartItemsState, totalItemsState, totalPriceState } from '../lib/cartState';
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+interface ProductImage {
+  thumbnails: {};
+  url: string;
+}
+
+
+export interface ProductItem {
+  unit_price: number;
+  title: string;
+  id:string;
+  img: string;
+}
+
+function ProductButton({ product }:{product: ProductItem}) {
+  const [cartItems, setCartItems] = useRecoilState(cartItemsState);
+  const [totalItems, setTotalItems] = useRecoilState(totalItemsState);
+  const [totalPrice, setTotalPrice] = useRecoilState(totalPriceState);
+
+  useEffect(() => {
+    const cartData = JSON.stringify({ cartItems, totalItems, totalPrice });
+    Cookies.set('cartData', cartData);
+  }, [cartItems, totalItems, totalPrice]);
+
+
+  const addToCart = () => {
+    const updatedCart = [...cartItems, product] as any;
+    setCartItems(updatedCart);
+    setTotalItems(totalItems + 1);
+    setTotalPrice(totalPrice + product.unit_price);
+  };
+
+  return (
+    <div>
+      <button onClick={addToCart} className="btn btn-primary">Agregar al carrito</button>
+    </div>
+  );
+}
+
+export default ProductButton;
